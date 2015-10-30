40ae410e35e5ddb06c1bc7867c8373015b5b34e6
Qi Zhao <zhaoq0563@gmail.com>
2015-10-29 18:32:09 -0700

    Change the code structure

--- src/scala/MarkDuplicates.scala
+++ src/scala/MarkDuplicates.scala
@@ -10,6 +10,10 @@
 
 package scala
 
+import htsjdk.samtools.SAMFileHeader
+import htsjdk.samtools.util.{SortingLongCollection, SortingCollection}
+import picard.sam.markduplicates.util.{LibraryIdGenerator, ReadEndsForMarkDuplicates}
+
 //import picard.DuplicationMetrics;
 //import picard.markduplicates.util.AbstractMarkDuplicatesCommandLineProgram;
 //import picard.markduplicates.util.LibraryIdGenerator;
@@ -18,11 +22,12 @@ package scala
 //import picard.markduplicates.util.ReadEndsForMarkDuplicatesCodec;
 
 //object MarkDuplicates extends AbstractMarkDuplicatesCommandLineProgram {
-object MarkDuplicates{    
-    //var pairSort = new SortingCollection[ReadEndsForMarkDuplicates]
-    //var fragSort = new SortingCollection[ReadEndsForMarkDuplicates]
-    //var dupliateIndexes = new SortingLongCollection
-    //var libraryIdGenerator = new LibraryIdGenerator
+class MarkDuplicates{
+
+    var pairSort = new SortingCollection[ReadEndsForMarkDuplicates]
+    var fragSort = new SortingCollection[ReadEndsForMarkDuplicates]
+    var dupliateIndexes = new SortingLongCollection(1000000000)
+    var libraryIdGenerator = new LibraryIdGenerator(SAMFileHeader)
 
     def main(args:Array[String]) = {
       var i = args(0)
