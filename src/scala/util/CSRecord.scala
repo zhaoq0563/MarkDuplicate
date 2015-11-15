8095d0688d0794283112830fb70d38306cd617b0
Qi Zhao <zhaoq0563@gmail.com>
2015-11-14 17:22:03 -0800

    Implement the functions

---
+++ src/scala/util/CSRecord.scala
@@ -0,0 +1,15 @@
+package scala.util
+
+/**
+ * Created by SpinyQ on 11/14/15.
+ */
+class CSAlignmentRecord {
+  // Single alignment record for building frag/pair Sort
+
+  // Flags needed for building frag/pair Sort
+  var referenceIndex : Integer = 0
+  var readNegativeStrandFlag : Boolean = false
+  var unclippedEnd : Int = 0
+
+
+}
