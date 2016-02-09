126077d30e5d492207dda139f9b3c1eba103a261
Qi Zhao <zhaoq0563@gmail.com>
2016-02-08 23:23:43 -0800

    Testing

--- src/main/scala/csadam/MarkDuplicates.scala
+++ src/main/scala/csadam/MarkDuplicates.scala
@@ -104,6 +104,7 @@ object MarkDuplicates extends AbstractMarkDuplicatesCommandLineProgram {
 
       // Collect the data from CSrdd and iterate them to build frag/pair Sort
       val readArray = readCSIndexRDD.collect()
+      println("The size of the read array is: " + readArray.length)
       //val readArray = readCSIndexRDD.take(10000)
 
       println("*** Finish collecting! ***\n")
